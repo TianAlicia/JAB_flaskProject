a0ce6bff12e2040821745933cc9a29c5726ef109
Tianzong <jiaoalicia8@gmail.com>
2023-05-03 21:28:54 +0200

    0101Project initialization(项目初始化)

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+# isort 是为了把文件按好的顺序整理一下
+# black 为了检查和校验语法规格
+# pre-commit如果前两步骤没有做或者通过不允许提交
+
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run()
