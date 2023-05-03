b054840f31b380b0c420d7ed965a5f1d3184435c
Tianzong <jiaoalicia8@gmail.com>
2023-05-03 21:49:58 +0200

    0102 Utilizar archivo de datos(Config)(使用配置文件)

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import json
-import os
-
+from config import config
 from flask import Flask
 
 app = Flask(__name__)
 # isort 是为了把文件按好的顺序整理一下
 # black 为了检查和校验语法规格
 # pre-commit如果前两步骤没有做或者通过不允许提交
+app.config.from_object(config["dev"])
 
 
 @app.route("/")
