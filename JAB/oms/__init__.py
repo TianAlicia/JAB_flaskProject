44bb62d599abe2a30adf1861855e5d5b8e3d612e
rebbant <bernibrunet7@gmail.com>
2023-06-01 10:16:02 +0200

    borro fitxersç

--- JAB/oms/__init__.py
+++
@@ -1,7 +0,0 @@
-from .article import ArticleORM
-from .categorg import CategoryORM
-from .chat import ChatORM
-from .comment import CommentORM
-from .commit_like import CommentLikeORM
-from .user import UserORM
-from .user_collection import UserCollectionORM
